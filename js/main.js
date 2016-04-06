2a07a3e7b060e7fadc388a55ba345040e5fd4b15
geekwise_veronica_chavez <veroch03@gmail.com>
2016-04-06 20:59:44 +0000

    remove dots

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var add_images= function(number_of_images, image_width){
         var image;
         for(var i=0; i< number_of_images; i++){
               image=document.createElement('img');
-              image_url.push('../images/profile_photo_' + (i + 1) + '.jpg');
+              image_url.push('images/profile_photo_' + (i + 1) + '.jpg');
               image.setAttribute('src', image_url[i]);
               
               document.body.appendChild(image);
